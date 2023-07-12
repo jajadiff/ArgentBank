748530f8b7f57cf59ee4c82daf3d8b71fa166b3b
Jordan <jordanfurneri57@gmail.com>
2023-07-12 15:11:28 +0200

    add img logo header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Router from "./pages/routes";
 import Footer from "./components/footer";
+import Header from "./components/header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Router />
-      </header>
+      <header className="App-header"> </header>
+      <Header />
+      <Router />
+
       <Footer />
     </div>
   );
--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "./style.css";
+import logo from "../../assets/img/logo.png";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <nav className="main-nav">
+      <a className="main-nav-logo" href="./index.html">
+        <img className="main-nav-logo-image" src={logo} alt="Argent Bank Logo" />
+        <h1 className="sr-only">Argent Bank</h1>
+      </a>
+      <div>
+        <a className="main-nav-item" href="./user.html">
+          <i className="fa fa-user-circle"></i>
+          Tony
+        </a>
+        <a className="main-nav-item" href="./index.html">
+          <i className="fa fa-sign-out"></i>
+          Sign Out
+        </a>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
